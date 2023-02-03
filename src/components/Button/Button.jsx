22b32ec037dd4ba42feb19a103ac05e7bec5b9f5
Danish <dan71ish@gmail.com>
2023-02-03 11:02:19 +0530

    navigation done

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './style.css';
+const Button = () => {
+  return (
+    <button className="px-4 py-2 text-white rounded-md btn">Get Benefits</button>
+    )
+}
+
+export default Button
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import img1 from '../assets/logo.png';
+import Button from '../Button/Button';
+import './navigation.css';
+const Navigation = () => {
+  return (
+    <nav className="navbar px-32 py-4 flex items-center justify-between">
+    <img src={img1} alt="Image"/>        
+    <ul className='font-medium'>
+        <li className='inline-block p-2 mr-8 text-xl '><a href="#">Home</a></li>
+        <li className='inline-block p-2 mr-8 text-xl'><a href="#">Offers</a></li>
+        <li className='inline-block p-2 mr-8 text-xl'><a href="#">WhyGetBenefit</a></li>
+        <li className='inline-block p-2 mr-8 text-xl'><a href="#">FAQs</a></li>
+    </ul>
+    <Button />
+    </nav>
+  )
+}
+
+export default Navigation
