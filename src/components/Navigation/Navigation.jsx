90c7cb1f55e7572eeee2bcaccc2a81b12278bbf4
Danish <dan71ish@gmail.com>
2023-02-04 11:21:07 +0530

    how it works section done

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,7 +4,6 @@ import './navigation.css';
 const Navigation = () => {
   return (
     <nav className="navbar flex items-center justify-between mt-6">
-    
     <img src="./assets/logo.png" alt="Image"/>        
     <ul className='font-medium'>
         <li className='inline-block mr-8 text-xl active'><a href="#" className='px-4 py-2 hover:text-white rounded-md active'>Home</a></li>
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
-
+import './work.css'
 const Work = () => {
     return (
         <>
-        <h1>How it Works</h1>
-        <div className="flex">
+        <div className='w-10/12 mx-auto mt-12'>
+        <h1 className='work-heading my-6 font-semibold'>How it Works</h1>
+        <div className="flex my-12 work-flex justify-between">
         <div className='work'>
-            <img src="" alt="" />
-            <h1>Onboard</h1>
+            <img src="./assets/onboard.svg" alt="" />
+            <h1 className='font-medium mt-9 mb-4 text-3xl'>Onboard</h1>
             <p>Sign up to get benefit and crate an account </p>
         </div>
         <div className='work'>
-            <img src="" alt="" />
-            <h1>Get Plans</h1>
+            <img src="./assets/get-plans.svg" alt="" />
+            <h1 className='font-medium mt-9 mb-4 text-3xl'>Get Plans</h1>
             <p>Browse our selection of comprehensive benefits options and select the ones that are right for you</p>
         </div>
         <div className='work'>
-            <img src="" alt="" />
-            <h1>Get Advice</h1>
+            <img src="./assets/get-advice.svg" alt="" />
+            <h1 className='font-medium mt-9 mb-4 text-3xl'>Get Advice</h1>
             <p>You can chose to speak with a representative who will advice you based on what plans or bundles you can opt for.</p>
         </div>
         <div className='work'>
-            <img src="" alt="" />
-            <h1>Get Benefits</h1>
+            <img src="./assets/get-benefits.svg" alt="" />
+            <h1 className='font-medium mt-9 mb-4 text-3xl'>Get Benefits</h1>
             <p>You make payment and start using your getbeneft voucher whenever you need to.</p>
+        </div>
         </div>
         </div>
         </>
