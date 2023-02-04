23e7f0d40e583ea0b560acca1eb4d92e416ce230
Danish <dan71ish@gmail.com>
2023-02-04 12:22:36 +0530

    spending component done

--- src/components/Spend/Spend.jsx
+++ src/components/Spend/Spend.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import './spend.css'
 const Spend = () => {
   return (
-    <div className='flex'>
-        <img src="" alt="" />
-        <div>“Spend more time being productive, save more and get the most benefits out of life.”</div>
+    <>
+    <div className='w-10/12 mx-auto my-20'>
+    <div className='flex justify-between men'>
+        <img src="./assets/men.jpg" alt="Men" />
+        <div className='men-text'>
+          <p>
+          “Spend more time being productive, save more and get the most benefits out of life.”
+          </p>
+          </div>
     </div>
+    </div>
+    </>
   )
 }
 
