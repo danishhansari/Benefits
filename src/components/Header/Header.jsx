91bc81fe72ab7a55db21d555c66fa51fa752a2ba
Danish <dan71ish@gmail.com>
2023-02-06 18:15:14 +0530

    mobile section started

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,16 @@ import Navigation from '../Navigation/Navigation';
 const Header = () => {
   return (
     <> 
-    <div className='w-9/12 mx-auto'>
+    <div className='md:w-9/12 w-full mx-auto pl-4 md:pl-0'>
     <Navigation /> 
-    <div className="flex justify-between hero pb-20">
+    <div className="md:flex justify-between hero pb-20 mt-20">
         <div>
-            <h1 className='heading heading-purple'>Bundled Benefit For The </h1>
-            <h1 className='heading heading-pink mb-9'>Modern Workforce</h1>
+            <h1 className='heading heading-purple text-4xl md:text-6xl font-semibold'>Bundled Benefit For The </h1>
+            <h1 className='heading heading-pink text-4xl md:text-6xl font-semibold md:mt-4'>Modern Workforce</h1>
+            <div className="my-16 md:my-6">
             <Button />
+            </div>
+
         </div>
         <div>
             <img src="./assets/hero.jpg" alt="Mens" />
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,15 +3,17 @@ import Button from '../Button/Button';
 import './navigation.css';
 const Navigation = () => {
   return (
-    <nav className="navbar flex items-center justify-between mt-6">
-    <img src="./assets/logo.png" alt="Image"/>        
-    <ul className='font-medium'>
+    <nav className="navbar flex items-center justify-between py-4 md:px-2">
+    <img src="./assets/logo.png" alt="Image" className="w-24"/>        
+    <ul className='font-medium hidden md:block'>
         <li className='inline-block mr-8 text-xl active'><a href="#" className='px-4 py-2 hover:text-white rounded-md active'>Home</a></li>
         <li className='inline-block mr-8 text-xl'><a href="#" className='px-4 py-2 hover:text-white rounded-md'>Offers</a></li>
         <li className='inline-block mr-8 text-xl'><a href="#" className='px-4 py-2 hover:text-white rounded-md'>WhyGetBenefit</a></li>
         <li className='inline-block mr-8 text-xl'><a href="#" className='px-4 py-2 hover:text-white rounded-md'>FAQs</a></li>
     </ul>
+    <div className="button hidden md:block">
     <Button />
+    </div>
     </nav>
   )
 }
