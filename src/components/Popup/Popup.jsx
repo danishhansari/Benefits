7e0a7f384da79b56665961a12a65a29319d02519
Danish <dan71ish@gmail.com>
2023-02-06 18:30:57 +0530

    header purple

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './popup.css';
 const Popup = () => {
   return (
-    <div className='text-center popup py-3 text-white font-medium text-sm md:text-2xl md:font-normal'>
+    <div className='text-center popup py-3 text-white font-medium text-xs px-1 md:text-2xl md:font-normal'>
         <p>With as little as <span className='text-2xl font-bold'>30,000</span> Naira, you get the benefits</p>
         <p className='text-center popup'>of working for a big company, even if you are not..</p>
     </div>
