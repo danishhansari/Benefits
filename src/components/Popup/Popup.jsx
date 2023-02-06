5407176d531b74b277eda0afad84a27bbf0b0103
Danish <dan71ish@gmail.com>
2023-02-06 18:24:32 +0530

    mobile section header done

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './popup.css';
 const Popup = () => {
   return (
-    <div className='text-center popup py-2 text-white font-medium'>
-        <p>With as little as <span className='text-3xl font-bold'>30,000</span> Naira, you get the benefits</p>
+    <div className='text-center popup py-3 text-white font-medium text-sm md:text-2xl md:font-normal'>
+        <p>With as little as <span className='text-2xl font-bold'>30,000</span> Naira, you get the benefits</p>
         <p className='text-center popup'>of working for a big company, even if you are not..</p>
     </div>
   )
