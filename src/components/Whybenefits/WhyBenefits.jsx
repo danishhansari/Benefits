43301525ee07214138ffb69b8c64e08ba2c45868
Danish <dan71ish@gmail.com>
2023-02-11 20:21:19 +0530

    whyBenefits section done

--- src/components/Whybenefits/WhyBenefits.jsx
+++ src/components/Whybenefits/WhyBenefits.jsx
@@ -3,17 +3,17 @@ import "./whyBenefits.css"
 const WhyBenefits = () => {
   return (
     <>
-    <div className="w-10/12 mx-auto why-benefit mb-16">
-      <div className="flex">
-        <div className="purple list-item w-full">
-          <button>Why Get Button?</button>
-        <ul>
-                <li><img src="./assets/right.svg" alt="" className='inline-block w-5 mr-1'/> Access Benefit in a single bundle</li>
-                <li><img src="./assets/right.svg" alt="" className='inline-block w-5 mr-1'/>Flexible Benefits Plans</li>
-                <li><img src="./assets/right.svg" alt="" className='inline-block w-5 mr-1'/>Secure and Compliance</li>
-                <li><img src="./assets/right.svg" alt="" className='inline-block w-5 mr-1'/>Pay-as-you go</li>
-                <li><img src="./assets/right.svg" alt="" className='inline-block w-5 mr-1'/>24/7 Support</li>
-                <li><img src="./assets/right.svg" alt="" className='inline-block w-5 mr-1'/>Zero paper work</li>
+    <div className="md:w-10/12 mx-auto why-benefit mb-16">
+      <div className="flex flex-col-reverse md:flex-row">
+        <div className="purple list-item md:w-1/2">
+          <button className='text-2xl md:text-4xl mt-12 ml-6 md:ml-16'>Why Get Button?</button>
+        <ul className='mb-8 md:mb-2 md:ml-16 ml-6'>
+                <li className='text-lg md:text-2xl'><img src="./assets/right.svg" alt="img" className='inline-block w-4 mr-3'/> Access Benefit in a single bundle</li>
+                <li className='text-lg md:text-2xl'><img src="./assets/right.svg" alt="img" className='inline-block w-4 mr-3'/>Flexible Benefits Plans</li>
+                <li className='text-lg md:text-2xl'><img src="./assets/right.svg" alt="img" className='inline-block w-4 mr-3'/>Secure and Compliance</li>
+                <li className='text-lg md:text-2xl'><img src="./assets/right.svg" alt="img" className='inline-block w-4 mr-3'/>Pay-as-you go</li>
+                <li className='text-lg md:text-2xl'><img src="./assets/right.svg" alt="img" className='inline-block w-4 mr-3'/>24/7 Support</li>
+                <li className='text-lg md:text-2xl'><img src="./assets/right.svg" alt="img" className='inline-block w-4 mr-3'/>Zero paper work</li>
             </ul>
         </div>
         <div className="img">
